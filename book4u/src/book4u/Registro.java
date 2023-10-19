3982d5c501d7d5d8a6936e16f0d182ffa3bd11c0
leorga044 <gr914087@alumnes.ilernalleida.com>
2023-10-19 17:37:51 +0200

    update registro

--- book4u/src/book4u/Registro.java
+++ book4u/src/book4u/Registro.java
@@ -13,14 +13,38 @@ import java.sql.SQLException;
 public class Registro extends JFrame {
 
 	static JTextField nombre;
+	 Font fuente = new Font("",Font.PLAIN,23);
 	public Registro(){
 		
 		setTitle("Registro");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		this.setSize(800, 500);
 		this.setLocationRelativeTo(null);
 		this.setVisible(true);
+		this.setLayout(null);
 		
 		
+		JPanel pan = new JPanel();
+		pan.setBounds(0, 0, 800, 50);
+		pan.setVisible(true);
+		pan.setBackground(Color.WHITE);
+		
+		ImageIcon imagen = new ImageIcon("Logo.png");
+		JLabel ima = new JLabel(imagen);
+		ima.setPreferredSize((new Dimension(200,0)));
+		this.add(ima);
+		
+		
+        String colorfondo = "#579514";
+        
+        Color backgroundColor = Color.decode(colorfondo);
+        this.getContentPane().setBackground(backgroundColor);
+        
+        JLabel tit = new JLabel("REGISTRO");
+        tit.setBounds(325, 0, 200, 20);
+        
+        tit.setFont(fuente);
+        this.add(tit);
+        this.add(pan);
 	}
 }
