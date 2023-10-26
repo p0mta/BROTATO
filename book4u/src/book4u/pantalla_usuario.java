06c2edc4047368784959fec58705100eb07907be
leorga044 <gr914087@alumnes.ilernalleida.com>
2023-10-26 19:20:31 +0200

    loco

--- book4u/src/book4u/pantalla_usuario.java
+++ book4u/src/book4u/pantalla_usuario.java
@@ -1,17 +1,65 @@
 package book4u;
 
+import java.awt.Color;
 import java.awt.Font;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
 public class pantalla_usuario extends JFrame {
 	
-	
+	static Font fuente = new Font("AGENCY FB", Font.BOLD,30);
+	static JTextField nombre;
+	static JPasswordField contra;
+	static JTextField correo;
 	public pantalla_usuario() {
+		
 		setTitle("Usuario");
-		setSize(800, 400);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(800, 500);
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
+		this.setLayout(null);
+		
+		JPanel pan = new JPanel();
+		pan.setBounds(0, 0, 800, 75);
+		pan.setVisible(true);
+		pan.setBackground(Color.WHITE);
+		pan.setLayout(null);
+		this.add(pan);
+		JLabel tit = new JLabel("USUARIO");
+        tit.setBounds(315, 20, 200, 40);
+        tit.setFont(Registro.fuente);
+        pan.add(tit);
+		
+		ImageIcon imagen = new ImageIcon("Logo.png");
+		JLabel ima = new JLabel(imagen);
+		ima.setBounds(0, 0, 150, 100); 
+		pan.add(ima);
+		
+		ImageIcon cir = new ImageIcon("circulo.png");
+		JLabel cir1 = new JLabel(cir);
+		cir1.setBounds(670, -10, 100, 100);
 		
 		
+		JLabel let = new JLabel(cir, Registro.nombre.getText().toUpperCase().charAt(1));
+		let.setBounds(713, 15, 50, 50);
+		let.setForeground(Color.white);
+		let.setFont(fuente);
+		pan.add(let);
+		pan.add(cir1);
+		 String colorfondo = "#579514";
+	        Color backgroundColor = Color.decode(colorfondo);
+	        this.getContentPane().setBackground(backgroundColor);
+		
+		nombre = new JTextField();
+		nombre.setBounds(140, 200, 200, 25);
+		nombre.setHorizontalAlignment(JTextField.CENTER);
+		this.add(nombre);
 		
 	}
 }
