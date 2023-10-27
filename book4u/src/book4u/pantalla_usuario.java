9294e2935c642714756f1e3bb393ebd7742fd2f1
leorga044 <gr914087@alumnes.ilernalleida.com>
2023-10-27 19:59:45 +0200

    siu

--- book4u/src/book4u/pantalla_usuario.java
+++ book4u/src/book4u/pantalla_usuario.java
@@ -2,11 +2,19 @@ package book4u;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.Statement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
@@ -14,7 +22,7 @@ import javax.swing.JTextField;
 public class pantalla_usuario extends JFrame {
 	
 	static Font fuente = new Font("AGENCY FB", Font.BOLD,50);
-	static JTextField nombre;
+	static JTextField nombre3;
 	static JPasswordField contra;
 	static JTextField correo;
 	public pantalla_usuario() {
@@ -36,11 +44,11 @@ public class pantalla_usuario extends JFrame {
         tit.setBounds(315, 20, 200, 40);
         tit.setFont(fuente);
         pan.add(tit);
-        nombre = new JTextField();
-		nombre.setBounds(100, 150, 200, 25);
-		nombre.setHorizontalAlignment(JTextField.CENTER);
-		nombre.setBorder(BorderFactory.createLineBorder(Color.black));
-		this.add(nombre);
+        nombre3 = new JTextField();
+		nombre3.setBounds(100, 150, 200, 25);
+		nombre3.setHorizontalAlignment(JTextField.CENTER);
+		nombre3.setBorder(BorderFactory.createLineBorder(Color.black));
+		this.add(nombre3);
 		JLabel nombre2 = new JLabel("CAMBIAR NOMBRE");
 		nombre2.setBounds(130, 120, 140, 30);
 		nombre2.setFont(Registro.fuente1);
@@ -60,16 +68,97 @@ public class pantalla_usuario extends JFrame {
 		char primeraLetra = nombreUsuario.charAt(0);
 
 		JLabel let = new JLabel(String.valueOf(primeraLetra));
-		let.setBounds(711, 15, 50, 50);
+		let.setHorizontalAlignment(JLabel.CENTER);
+		let.setBounds(696, 15, 50, 50);
 		let.setForeground(Color.white);
 		let.setFont(fuente);
 		pan.add(let);
 		pan.add(cir1);
+		
+		ImageIcon icono = new ImageIcon("change.png");
+        
+        // Crear un botón y establecer la imagen y el texto
+        JButton change = new JButton(icono);
+        change.setBounds(118, 180, 150, 30);
+        change.setFont(Registro.fuente1);
+        change.setText("CHANGE"); // Establece el texto del botón
+        change.setFocusPainted(false);
+        change.setBorderPainted(false);
+        change.setContentAreaFilled(false);
+        this.add(change);
+		
 		 String colorfondo = "#579514";
 	        Color backgroundColor = Color.decode(colorfondo);
 	        this.getContentPane().setBackground(backgroundColor);
 		
 		
-		
-	}
+	        change.addActionListener(new ActionListener() {
+	            public void actionPerformed(ActionEvent e) {
+	                String correo = Login.usernameField.getText();
+	                String contrasena = String.valueOf(Login.getPasswordField().getPassword());
+
+	                // Obtener el nombre de usuario desde la base de datos
+	                String nombre = obtenerNombreDesdeBaseDeDatos(correo, contrasena);
+	                if(nombre3.getText().isEmpty()) {
+	                	JOptionPane.showMessageDialog(pantalla_usuario.this, "Tienes que rellenar el campo nombre");
+	                	return;
+	                }
+	                if (nombre != null) {
+	                    // Mostrar un cuadro de diálogo para que el usuario ingrese el nuevo nombre
+	                    
+
+	                    if (nombre2.getText() != null && !nombre2.getText().isEmpty()) {
+	                        // Realizar la actualización con el nuevo nombre
+	                        String update = "UPDATE USUARIO SET NOMBRE = ? WHERE CORREO = ? AND CONTRASEÑA = ?";
+	                        try {
+	                            PreparedStatement preparedStatement = Login.connection.prepareStatement(update);
+	                            preparedStatement.setString(1, nombre3.getText());
+	                            preparedStatement.setString(2, correo);
+	                            preparedStatement.setString(3, contrasena);
+
+	                            int rowsUpdated = preparedStatement.executeUpdate();
+
+	                            if (rowsUpdated > 0) {
+	                                JOptionPane.showMessageDialog(pantalla_usuario.this, "Nombre de usuario modificado correctamente");
+	                            } else {
+	                                JOptionPane.showMessageDialog(pantalla_usuario.this, "No se pudo modificar el nombre de usuario. Verifica que la contraseña actual sea correcta.");
+	                            }
+	                        } catch (SQLException ex) {
+	                            ex.printStackTrace();
+	                            JOptionPane.showMessageDialog(pantalla_usuario.this, "Error al modificar el nombre de usuario");
+	                        }
+	                    } else {
+	                        JOptionPane.showMessageDialog(pantalla_usuario.this, "Nombre de usuario no válido.");
+	                    }
+	                } else {
+	                    JOptionPane.showMessageDialog(pantalla_usuario.this, "No se pudo obtener el nombre de usuario desde la base de datos.");
+	                }
+	                
+	            }
+	        });
+	    }
+
+	    // Método para obtener el nombre desde la base de datos
+	    
+
+	
+private String obtenerNombreDesdeBaseDeDatos(String correo, String contraseña) {
+    String nombre = null;
+    try {
+        String selectQuery = "SELECT NOMBRE FROM USUARIO WHERE CORREO = ? AND CONTRASEÑA = ?";
+        PreparedStatement preparedStatement = Login.connection.prepareStatement(selectQuery);
+        preparedStatement.setString(1, correo);
+        preparedStatement.setString(2, contraseña);
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        if (resultSet.next()) {
+            nombre = resultSet.getString("NOMBRE");
+        }
+
+        resultSet.close();
+    } catch (SQLException ex) {
+        ex.printStackTrace();
+    }
+    return nombre;
+}
 }
