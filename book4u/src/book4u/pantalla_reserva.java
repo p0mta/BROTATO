bd2c8f0ad343ce1c16a6b6ebbb32ee6145878c6d
Ivama123 <ivantorn2017@gmail.com>
2023-11-06 20:05:10 +0100

    pantalla

--- book4u/src/book4u/pantalla_reserva.java
+++ book4u/src/book4u/pantalla_reserva.java
@@ -5,59 +5,97 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import com.toedter.calendar.JDateChooser;
+import java.util.Date;
 
 public class pantalla_reserva extends JFrame {
     public pantalla_reserva() {
         setTitle("Reserva de lugares residenciales");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(800, 500);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setLayout(null);
+
         String colorFondo = "#579514";
         Color backgroundColor = Color.decode(colorFondo);
         getContentPane().setBackground(backgroundColor);
 
-        // Centrar la ventana en la pantalla
-        setLocationRelativeTo(null);
+        JPanel panelBlanco = new JPanel();
+        panelBlanco.setBounds(0, 0, 800, 75);
+        panelBlanco.setVisible(true);
+        panelBlanco.setBackground(Color.WHITE);
+        panelBlanco.setLayout(null);
+        add(panelBlanco);
+
+        JLabel tit = new JLabel("PANTALLA RESERVA");
+        tit.setFont(Registro.fuente);
+        tit.setBounds(225, 0, 400, 75);
+        panelBlanco.add(tit);
 
         ImageIcon imagen = new ImageIcon("logo.png");
         JLabel label = new JLabel(imagen);
-        label.setPreferredSize(new Dimension(450, 100));
+        label.setBounds(0, 0, 150, 100);
+        panelBlanco.add(label);
+
+        JLabel lugarLabel = new JLabel("Lugar Residencial:");
+        lugarLabel.setBounds(200, 150, 150, 30);
+        add(lugarLabel);
+        lugarLabel.setFont(Registro.fuente1);
         
+
         String[] lugaresResidenciales = {"Casa", "Apartamento", "Cabaña"};
         JComboBox<String> lugarResidencialCombo = new JComboBox<>(lugaresResidenciales);
+        lugarResidencialCombo.setBounds(350, 150, 200, 30);
+        add(lugarResidencialCombo);
+        
+        
+
+        JLabel fechaLabel = new JLabel("Fecha de Reserva:");
+        fechaLabel.setBounds(200, 200, 150, 30);
+        add(fechaLabel);
+        fechaLabel.setFont(Registro.fuente1);
+        
 
         JDateChooser dateChooser = new JDateChooser();
+        dateChooser.setBounds(350, 200, 200, 30);
+        add(dateChooser);
+        dateChooser.setBackground(Color.white);
+        dateChooser.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 
         JLabel precioLabel = new JLabel("Precio de la reserva: $0.00");
+        precioLabel.setBounds(200, 250, 350, 30);
+        add(precioLabel);
+        precioLabel.setFont(Registro.fuente1);
 
         JButton calcularPrecioButton = new JButton("Calcular Precio");
+        calcularPrecioButton.setBounds(200, 300, 150, 30);
+        add(calcularPrecioButton);
+        calcularPrecioButton.setFont(Registro.fuente1);
+        calcularPrecioButton.setBackground(Color.white);
+        calcularPrecioButton.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 
         calcularPrecioButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String lugarResidencial = lugarResidencialCombo.getSelectedItem().toString();
-                java.util.Date selectedDate = dateChooser.getDate();
-                // Aquí calcularías el precio según los datos seleccionados
+                Date selectedDate = dateChooser.getDate();
+                Date today = new Date();  // Fecha actual
 
-                precioLabel.setText("Precio de la reserva: $XXX.XX"); // Reemplaza XXX.XX con el precio real
+                if (selectedDate.before(today)) {
+                    JOptionPane.showMessageDialog(null, "No se puede reservar para una fecha anterior al día actual", "Error", JOptionPane.ERROR_MESSAGE);
+                } else {
+                    // Aquí calcularías el precio según los datos seleccionados
+                    precioLabel.setText("Precio de la reserva: $XXX.XX"); // Reemplaza XXX.XX con el precio real
+                }
             }
         });
-
-        setLayout(new FlowLayout(FlowLayout.CENTER, 20, 20));
-        add(label);
-        add(new JLabel("Lugar Residencial:"));
-        add(lugarResidencialCombo);
-        add(new JLabel("Fecha de Reserva:"));
-        add(dateChooser);
-        add(calcularPrecioButton);
-        add(precioLabel);
     }
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-            	pantalla_reserva frame = new pantalla_reserva();
-                frame.setVisible(true);
+                pantalla_reserva frame = new pantalla_reserva();
             }
         });
     }
